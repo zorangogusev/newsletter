a8d67b3ee632599d26c78e3321d3cbd9612405cf
zoran <zoran_gogusev@yahoo.com>
2020-12-15 05:25:49 +0100

    Heroku

--- resources/js/react/src/createStore.js
+++ resources/js/react/src/createStore.js
@@ -4,6 +4,7 @@ import RootReducer from "./store/reducers/RootReducer";
 
 export const middlewares = [thunk];
 
-export const createStoreWithMiddleware = compose(applyMiddleware(...middlewares), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())(createStore);
+// export const createStoreWithMiddleware = compose(applyMiddleware(...middlewares), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())(createStore);
+export const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
 
 export const store = createStoreWithMiddleware(RootReducer);
